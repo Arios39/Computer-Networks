2febcd3bb335949477532fdaf2ed169c5808f599
cgOrtiz805 <ortizcristian1260@gmail.com>
2020-02-01 19:44:14 -0800

    keeping it PG :)

--- script1.py
+++ script1.py
@@ -23,7 +23,7 @@ s.addChannel(s.GENERAL_CHANNEL);
 
     # After sending a ping, simulate a little to prevent collision.
 s.runTime(1);
-s.ping(1, 2, "Fuck");
+s.ping(1, 2, "Heck"); //Keep it PG lol
 s.runTime(1);
 
 s.ping(2, 3, "Hi!");
